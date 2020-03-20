ea29dae475e98a1a3e68c35ccdc454e55fcd863e
Jasper Song <jaspersong000@gmail.com>
2020-03-19 20:47:46 -0400

    Created trivial application

---
+++ tests/trivial_app_nonet.cpp
@@ -0,0 +1,19 @@
+/**
+ * Name: Snowy Chen, Joe Song
+ * Date: 21 February 2020
+ * Section: Jason Hemann, MR 11:45-1:25
+ * Email: chen.xinu@husky.neu.edu, song.jo@husky.neu.edu
+ */
+
+// Lang::CwC
+
+#include "trivial_app.h"
+
+int main(int argc, char **argv) {
+  Trivial demo;
+
+  demo.start();
+  demo.join();
+
+  return 0;
+}
